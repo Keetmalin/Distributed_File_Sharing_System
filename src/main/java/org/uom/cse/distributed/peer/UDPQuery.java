b32501fd8149d05b15553cd4f0276d763fa6e78f
Keet Sugathadasa <keetmalin.13@cse.mrt.ac.lk>
2017-10-24 08:11:48 +0530

    Added initial files for the Query System

---
+++ src/main/java/org/uom/cse/distributed/peer/UDPQuery.java
@@ -0,0 +1,12 @@
+package org.uom.cse.distributed.peer;
+
+import org.uom.cse.distributed.peer.api.QueryInterface;
+
+/**
+ * This class implements the query part of the system where nodes can query
+ * for the files in need and do a look up
+ *
+ * @author Keet Sugathadasa
+ */
+public class UDPQuery implements QueryInterface {
+}
