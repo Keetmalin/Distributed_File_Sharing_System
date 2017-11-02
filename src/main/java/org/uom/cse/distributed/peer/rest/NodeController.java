79620ffdbfe2402909708c367ef69b987a938dd4
jayancv <jcvidanapathirana@gmail.com>
2017-11-02 14:00:54 +0530

    Rest server added

---
+++ src/main/java/org/uom/cse/distributed/peer/rest/NodeController.java
@@ -0,0 +1,56 @@
+package org.uom.cse.distributed.peer.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.uom.cse.distributed.peer.Node;
+import org.uom.cse.distributed.peer.api.EntryTableEntry;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/nodecontroller")
+public class NodeController {
+
+    private static final Logger logger = LoggerFactory.getLogger(NodeController.class);
+    private Node node;
+
+    @GET
+    @Path("/RouteTable")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response RouteTable() {
+        try {
+            return Response.ok(node.getRoutingTable().getEntries()).build();
+        } catch (Exception e) {
+            logger.error("Error occurred when building object request: {}", e);
+            return Response.status(500).build();
+        }
+    }
+
+    @GET
+    @Path("/NewNode/{id}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response NewNode(@PathParam("id") int id) {
+        try {
+            return Response.ok(node.getEntriesToHandoverTo(id)).build();
+        } catch (Exception e) {
+            logger.error("Couldn't find characters to be handed over to node -> {}", id);
+            return Response.status(500).build();
+        }
+    }
+
+    @POST
+    @Path("/NewEntry/{keyWord}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response NewEntry(EntryTableEntry entry, @PathParam("keyWord") String key) {
+        node.getEntryTable().addEntry(key, entry);
+        return Response.status(200).build();
+    }
+
+    @GET
+    @Path("/Query/{keyWord}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response Query(@PathParam("keyWord") String key) {
+        return Response.ok(node.getEntryTable().getEntriesByKyeword(key)).build();
+    }
+}
---
+++ src/main/java/org/uom/cse/distributed/peer/rest/RestNodeServer.java
@@ -0,0 +1,74 @@
+package org.uom.cse.distributed.peer.rest;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.uom.cse.distributed.peer.Node;
+import org.uom.cse.distributed.peer.api.NodeServer;
+
+import java.util.concurrent.ExecutorService;
+
+import static org.uom.cse.distributed.Constants.RETRIES_COUNT;
+
+/**
+ * This Class provides the implementation of the server side, of each of the nodes.
+ *
+ * @author Vithusha Aarabhi
+ * @author Jayan Vidanapathirana
+ */
+public class RestNodeServer implements NodeServer {
+
+    private static final Logger logger = LoggerFactory.getLogger(RestNodeServer.class);
+    private final int numOfRetries = RETRIES_COUNT;
+    private ExecutorService executorService;
+    private boolean started = false;
+
+    private Node node;
+    private Server jettyServer;
+
+    @Override
+    public void start(Node node) {
+        this.node = node;
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        jettyServer = new Server(node.getPort());
+        jettyServer.setHandler(context);
+
+        NodeController nodeController = new NodeController();
+        ServletHolder jerseyServlet = context.addServlet(
+                org.glassfish.jersey.servlet.ServletContainer.class, "/*");
+        jerseyServlet.setInitOrder(0);
+
+        // Tells the Jersey Servlet which REST service/class to load.
+        jerseyServlet.setInitParameter("jersey.config.server.provider.classnames",
+                NodeController.class.getCanonicalName());
+
+        try {
+            jettyServer.start();
+        } catch (Exception e) {
+            logger.error("Error occurred when starting REST server due to : {}", e.getMessage());
+        }
+
+        logger.info("REST Server started successfully ...");
+    }
+
+    @Override
+    public void stop() {
+        if (started) {
+            started = false;
+            try {
+                jettyServer.stop();
+            } catch (Exception e) {
+                logger.error("Error occurred when stopping the REST server due to : {}", e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void listen() {
+
+    }
+
+}
